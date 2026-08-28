3fabbcb6fba23d7e9a590b56b8252cca04cbde7f
balvarez <balvarez@usm.cl>
2026-08-28 17:08:53 -0400

    avance hasta minuto 18 app

--- src/components/JuegosForm.jsx
+++ src/components/JuegosForm.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Card, CardHeader,CardContent,TextField,CardActions,Button } from '@mui/material'
+import { Card, CardHeader,CardContent,TextField,CardActions,Button, Select, MenuItem } from '@mui/material'
 
 function JuegosForm() {
+        const companias = [{ label: "Sony", value: "sony" },
+             { label: "Microsoft", value:"microsoft" },
+             { label: "Nintendo",  value: "nintendo" }];
     return(
         <Card raised>
             <CardHeader title="Registrar juegos" >  </CardHeader>
             <CardContent>
                 <div className='mt-3'>
-                    sssdsdsdsdsdsdsdsds
                     <TextField label="Nombre" fullWidth id='nombre-juego'></TextField>
                 </div>
                 <div className="mt-3">
                     <TextField multiline label="Descripcion" fullWidth id='desc-juego'></TextField>
+                </div>
+                <div className="mt-3">
+                    <Select id='compania-juego'  label="Compañia">
+                        {companias.map((c)=> <MenuItem value={c.value}>{c.label}</MenuItem>
+                    )}
+                    </Select>
                 </div>
             </CardContent>
             <CardActions>
